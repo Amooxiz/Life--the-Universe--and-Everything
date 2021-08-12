f4bbc8ecf96d43b206bf5387a0ed2b11048e6c7d
Szymon Fiedosiuk <szymonfiedosiuk@wp.pl>
2021-08-12 07:35:26 +0200

    Code

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TEST___Life__the_Universe__and_Everything
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input;
+            input = Convert.ToInt32(Console.ReadLine());
+            int i = 0;
+            while (input != 42)
+            {
+                Console.WriteLine(input);
+                input = Convert.ToInt32(Console.ReadLine());
+                i++;
+            }
+
+        }
+    }
+}
